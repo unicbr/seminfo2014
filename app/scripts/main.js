f619d2b9032e1b26bc0a4a0224b832ed41e3c608
Gabriel Pedro <gpedro831@gmail.com>
2014-09-19 08:06:49 -0400

    Add Calendar Feature

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+
+  $('#agenda .w-tab-link').click(function() {
+    var year = $.fullCalendar.moment('2014-11-' + $(this).attr('data-w-tab'));
+    $('#agenda .w-tab-pane').fullCalendar('gotoDate', year);
+  });
+
+  $('#agenda .w-tab-pane').each(function(index, item) {
+    $(item)
+    .fullCalendar({
+      header: false,
+      lang: 'pt-br',
+      eventSources: [
+          'data/palestras.json',
+          'data/minicursos.json'
+      ]
+    })
+    .fullCalendar('changeView', 'agendaDay');
+  });
+
+  var year = $.fullCalendar.moment('2014-11-03');
+  $('#agenda .w-tab-pane').fullCalendar('gotoDate', year);
+
+});
