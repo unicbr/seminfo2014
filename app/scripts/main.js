6d5eca4e7b7ad24942a908582ea1723ca2554935
Gabriel Pedro <gabriel@rcfinovacoes.com.br>
2014-09-19 13:08:32 -0400

    Added Calendar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,64 @@
 $(function() {
 
+  function getEvents(file) {
+    $.getJSON('../data/' + file, function(response) {
+
+      return response;
+    });
+  }
+
   $('#agenda .w-tab-link').click(function() {
-    var year = $.fullCalendar.moment('2014-11-' + $(this).attr('data-w-tab'));
+    $('#agenda .w-tab-link').each(function(index, item) {
+      var obj = $(item);
+      if(obj.hasClass('w--current')) {
+        obj.removeClass('w--current');
+      }
+    });
+
+    !$(this).hasClass('w--current') && $(this).addClass('w--current');
+
+    var year = $.fullCalendar.moment('2014-11-' + $(this).attr('data-day'));
     $('#agenda .w-tab-pane').fullCalendar('gotoDate', year);
   });
 
-  $('#agenda .w-tab-pane').each(function(index, item) {
-    $(item)
+  $('#agenda .w-tab-pane')
     .fullCalendar({
       header: false,
+      allDaySlot: false,
       lang: 'pt-br',
+      defaultView: 'agendaDay',
       eventSources: [
-          'data/palestras.json',
-          'data/minicursos.json'
-      ]
-    })
-    .fullCalendar('changeView', 'agendaDay');
-  });
+        {
+            events: function(start, end, timezone, callback) {
+              $.ajax({
+                url: '../data/palestras.json',
+                dataType: 'json',
+                success: function(doc) {
+                    callback(doc);
+                }
+              });
+            },
+            color: '#003759',     // an option!
+            textColor: 'white' // an option!
+        },
+        {
+          events: function(start, end, timezone, callback) {
+            $.ajax({
+              url: '../data/minicursos.json',
+              dataType: 'json',
+              success: function(doc) {
+                  callback(doc);
+              }
+            });
+          },
+          color: '#00569f',     // an option!
+          textColor: 'white' // an option!
+        }
 
-  var year = $.fullCalendar.moment('2014-11-03');
+    ]
+    });
+
+  var year = $.fullCalendar.moment('2014-11-03T08:00:00-04:00');
   $('#agenda .w-tab-pane').fullCalendar('gotoDate', year);
 
 });
