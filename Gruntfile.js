8f073b2d7266ef5e9812601357c73171976400f8
Gabriel Pedro <gabriel@rcfinovacoes.com.br>
2014-09-19 13:41:29 -0400

    Cleaning Project

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,7 +285,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'data/{,*/}*.json'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
